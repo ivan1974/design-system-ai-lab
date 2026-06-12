e31cd49f062833cbcb8adad5ff2cd36e59a521b4
Ivan Lenoble <ivanlenoble@me.com>
2026-06-12 18:03:02 +0200

    feat: export ViewFilterBar component

--- src/design-system/components/index.ts
+++ src/design-system/components/index.ts
@@ -2,3 +2,5 @@ export { InstalledBaseHeader } from './installed-base-header';
 export type { InstalledBaseHeaderProps } from './installed-base-header';
 export { MainNavigation } from './main-navigation';
 export type { MainNavigationProps } from './main-navigation';
+export { ViewFilterBar } from './view-filter-bar';
+export type { ViewFilterBarProps, ViewFilterBarView } from './view-filter-bar';
