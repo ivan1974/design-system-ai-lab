56fcc63e6dbc15d9ca238328cd9ab02613838eba
Ivan Lenoble <ivanlenoble@me.com>
2026-06-12 17:59:37 +0200

    feat: export InstalledBaseHeader component

--- src/design-system/components/index.ts
+++ src/design-system/components/index.ts
@@ -1,2 +1,4 @@
+export { InstalledBaseHeader } from './installed-base-header';
+export type { InstalledBaseHeaderProps } from './installed-base-header';
 export { MainNavigation } from './main-navigation';
 export type { MainNavigationProps } from './main-navigation';
