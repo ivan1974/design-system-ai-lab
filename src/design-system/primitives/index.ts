4057c1731fd0353f0a95f775c26380ddebcbeb5c
Ivan Lenoble <ivanlenoble@me.com>
2026-06-12 17:43:14 +0200

    feat: export Tag primitive

--- src/design-system/primitives/index.ts
+++ src/design-system/primitives/index.ts
@@ -1,6 +1,8 @@
-export { Badge } from './badge';
-export type { BadgeProps } from './badge';
 export { Button } from './button';
 export type { ButtonProps } from './button';
+export { Badge } from './badge';
+export type { BadgeProps } from './badge';
 export { Pill } from './pill';
 export type { PillProps } from './pill';
+export { Tag } from './tag';
+export type { TagProps } from './tag';
