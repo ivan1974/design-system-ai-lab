b195ddf8d8b44d20c8d35dc5f2642fccec48930c
Ivan Lenoble <ivanlenoble@me.com>
2026-06-12 17:18:53 +0200

    feat: export Pill primitive

--- src/design-system/primitives/index.ts
+++ src/design-system/primitives/index.ts
@@ -2,3 +2,5 @@ export { Badge } from './badge';
 export type { BadgeProps } from './badge';
 export { Button } from './button';
 export type { ButtonProps } from './button';
+export { Pill } from './pill';
+export type { PillProps } from './pill';
