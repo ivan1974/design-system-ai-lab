6ae4fd0576b9f7dca1af3940975b3b31bb9ec9cc
Ivan Lenoble <ivanlenoble@me.com>
2026-06-13 09:57:58 +0200

    refactor: export promoted form primitives

--- src/design-system/primitives/index.ts
+++ src/design-system/primitives/index.ts
@@ -1,7 +1,27 @@
-export { Button } from './button';
-export type { ButtonProps } from './button';
 export { Badge } from './badge';
 export type { BadgeProps } from './badge';
+export { Button } from './button';
+export type { ButtonProps } from './button';
+export { Checkbox } from './checkbox';
+export {
+  DropdownMenu,
+  DropdownMenuPortal,
+  DropdownMenuTrigger,
+  DropdownMenuContent,
+  DropdownMenuGroup,
+  DropdownMenuLabel,
+  DropdownMenuItem,
+  DropdownMenuCheckboxItem,
+  DropdownMenuRadioGroup,
+  DropdownMenuRadioItem,
+  DropdownMenuSeparator,
+  DropdownMenuShortcut,
+  DropdownMenuSub,
+  DropdownMenuSubTrigger,
+  DropdownMenuSubContent,
+} from './dropdown-menu';
+export { Input } from './input';
+export type { InputProps } from './input';
 export { Pill } from './pill';
 export type { PillProps } from './pill';
 export { Tag } from './tag';
